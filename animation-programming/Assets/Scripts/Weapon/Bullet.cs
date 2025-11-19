89510ab1c3586837843d9043fe497eb65b247b83
Nina Mäkinen <sherlocked1223@gmail.com>
2025-11-19 02:41:40 +0200

    Fixed bullets going into a wrong direction

--- animation-programming/Assets/Scripts/Weapon/Bullet.cs
+++ animation-programming/Assets/Scripts/Weapon/Bullet.cs
@@ -4,6 +4,7 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _forceAmount = 10f;
     [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _lifeTime = 10f;
 
     private void Awake()
     {
@@ -15,5 +16,18 @@ public class Bullet : MonoBehaviour
         _rb.AddForce(transform.forward * _forceAmount, ForceMode.Impulse);    
     }
 
-    
+    private void Update()
+    {
+        _lifeTime -= Time.deltaTime;
+        if (_lifeTime <= 0) 
+        {
+            BulletDespawn();
+        }
+    }
+
+    private void BulletDespawn()
+    {
+        GameObject.Destroy(gameObject); //Make pooling later
+    }
+
 }
